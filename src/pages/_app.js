72d901de9403437661ed4115f6d53a6fbbd8051a
bruno.bozzetti <bruno.bozzetti@dbserver.com.br>
2023-03-10 18:02:41 -0300

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
-import '@/styles/globals.css'
+// import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import "@/styles/theme.scss";
+import "@/styles/globals.scss";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
---
+++ src/pages/pokemon/index.js
@@ -0,0 +1,9 @@
+function Pokemon() {
+  return (
+    <div>
+      <h1>Fucking Pokemon</h1>
+    </div>
+  );
+}
+
+export default Pokemon;
